3f3e0c141b89954231489b7aeb84ed33da210ccf
rongirl <gro.pretty.arabella@mail.ru>
2022-09-27 11:26:53 +0300

    Added task

---
+++ homework2/Lazy/Lazy/Lazy.cs
@@ -0,0 +1,37 @@
+﻿namespace Task_2;
+
+using System;
+
+/// <summary>
+/// Класс, реализующий ленивое вычисление в однопоточном режиме
+/// </summary>
+public class Lazy<T> : ILazy<T>
+{
+    private bool isCalculated = false;
+
+    /// <summary>
+    /// Функция вычисления
+    /// </summary>
+    private Func<T?> supplier;
+
+    /// <summary>
+    /// Результат вычисления
+    /// </summary>
+    private T? result;
+
+    /// <summary>
+    /// Инициализация экземпляра класса 
+    /// </summary>
+    public Lazy(Func<T?> supplier) => this.supplier = supplier;
+
+    public T? Get()
+    {
+        if (isCalculated)
+        {
+            return result;
+        }
+        result = supplier();
+        isCalculated = true;
+        return result;
+    }
+}
---
+++ homework2/Lazy/Lazy/LazyWithThreads.cs
@@ -0,0 +1,45 @@
+﻿namespace Task_2;
+
+using System;
+
+/// <summary>
+/// Класс, реализующий ленивое вычисление в многопоточном режиме
+/// </summary>
+public class LazyWithThreads<T> : ILazy<T>
+{   
+    private bool isCalculated = false;
+
+    /// <summary>
+    /// Функция вычисления
+    /// </summary>
+    private Func<T?> supplier;
+
+    /// <summary>
+    /// Результат вычисления
+    /// </summary>
+    private T? result;
+    private object locker = new object();
+
+    /// <summary>
+    /// Инициализация экземпляра класса 
+    /// </summary>
+    public LazyWithThreads(Func<T?> supplier) => this.supplier = supplier;
+
+    public T? Get()
+    {
+        if (isCalculated)
+        {
+            return result;
+        }
+        lock (locker)
+        {
+            if (isCalculated)
+            {
+                return result;
+            }
+            result = supplier();
+            isCalculated = true;
+            return result;
+        }
+    }
+}
