f4a31ec8f3a5dd7eeb48833b8062b41ece46a0d5
rongirl <gro.pretty.arabella@mail.ru>
2022-10-26 23:58:58 +0300

    inital commit

---
+++ homework3/MyThreadPool/MyThreadPool/IMyTask.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3;
+public interface IMyTask<TResult>
+{
+    public bool IsCompleted { get; }
+
+    public TResult Result { get; }
+
+    public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuationFunction);
+}
---
+++ homework3/MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,149 @@
+﻿namespace Task_3;
+
+using System.Collections.Concurrent;
+
+public class MyThreadPool
+{
+    private Thread[] threads;
+    private CancellationTokenSource cancellationToken;
+    private ConcurrentQueue<Action> actions;
+    private AutoResetEvent autoReset = new AutoResetEvent(false);   
+    private object lockObject = new();
+    public MyThreadPool(int countOfThreads)
+    {   
+        if (countOfThreads <= 0)
+        {
+            throw new ArgumentOutOfRangeException("Count must be positive");
+        }
+        cancellationToken = new CancellationTokenSource();  
+        actions = new ConcurrentQueue<Action>(); 
+        threads = new Thread[countOfThreads];
+        for (int i = 0; i < threads.Length; i++)
+        {
+            threads[i] = CreateThread();
+            threads[i].Start();
+        }
+    }
+
+    public IMyTask<TResult> Submit<TResult> (Func<TResult> function)
+    {
+        if (!cancellationToken.IsCancellationRequested)
+        {
+            lock(lockObject)
+            {   
+                var task = new MyTask<TResult>(function, this);
+                actions.Enqueue(task.Run);
+                autoReset.Set();
+                return task;
+            }
+        }
+        else
+        {
+            throw new InvalidOperationException();
+        }
+    }
+
+    public void ShutDown()
+    {
+
+    }
+
+    private Thread CreateThread()
+    {
+        var thread = new Thread(() =>
+        {   
+            autoReset.WaitOne();
+            if (actions.TryDequeue(out var action))
+            {
+                action();
+            }
+        }
+        );
+        return thread;
+    }
+
+    private class MyTask<TResult> : IMyTask<TResult>
+    {   
+        private Exception caughtException;
+        private Func<TResult> function;
+        private MyThreadPool threadPool;
+        private object lockObj = new object();   
+        private TResult result;
+        private ManualResetEvent isCalculated = new ManualResetEvent(false);
+        private ConcurrentQueue<Action> continuationOfTask = new ConcurrentQueue<Action>();
+
+        public bool IsCompleted { get; private set; }
+
+        public MyTask(Func<TResult> function, MyThreadPool threadPool)
+        {
+            this.function = function;
+            this.threadPool = threadPool;
+        }
+
+        public TResult Result
+        {
+            get 
+            {   
+                if (!IsCompleted)
+                {
+                    isCalculated.WaitOne();
+                }
+                if (caughtException != null)
+                {
+                    throw new AggregateException(caughtException);  
+                }
+                return result;
+            }
+        }
+
+        public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuationFunction)
+        {
+            if(threadPool.cancellationToken.IsCancellationRequested)
+            {
+                throw new InvalidOperationException();
+            }
+            lock(lockObj)
+            {
+                if (IsCompleted)
+                {
+                    threadPool.Submit(() => continuationFunction(result));
+                }
+                var newContinuationOfTask = new MyTask<TNewResult>(() => continuationFunction(result), threadPool);
+                continuationOfTask.Enqueue(newContinuationOfTask.Run);
+                return newContinuationOfTask;   
+            }
+        }
+
+        public void Run()
+        {
+            lock (lockObj)
+            {
+                try
+                {
+                    result = function();
+                    IsCompleted = true;
+                    isCalculated.Set();
+                    while (!continuationOfTask.IsEmpty)
+                    {
+                        if (continuationOfTask.TryDequeue(out var taskRun))
+                        {
+                            threadPool.actions.Enqueue(taskRun); //как работают потоки с задачи в тредпуле 
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    caughtException = ex;   
+                }
+
+            }
+        }
+
+
+
+        
+        
+    }
+
+    
+}
