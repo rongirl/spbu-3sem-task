dbebf4b38546de2a1c3291403b5d8ebc758a70a8
rongirl <gro.pretty.arabella@mail.ru>
2022-11-02 18:58:05 +0300

    add tests

--- homework3/MyThreadPool/MyThreadPool/IMyTask.cs
+++ homework3/MyThreadPool/MyThreadPool/IMyTask.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_3;
+﻿namespace Task_3;
 public interface IMyTask<TResult>
 {
     public bool IsCompleted { get; }
--- homework3/MyThreadPool/MyThreadPool/MyThreadPool.cs
+++ homework3/MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -6,17 +6,17 @@ public class MyThreadPool
 {
     private Thread[] threads;
     private CancellationTokenSource cancellationToken;
-    private ConcurrentQueue<Action> actions;
-    private AutoResetEvent autoReset = new AutoResetEvent(false);   
+    private BlockingCollection<Action> actions;
+    private AutoResetEvent autoReset = new AutoResetEvent(false);
     private object lockObject = new();
     public MyThreadPool(int countOfThreads)
-    {   
+    {
         if (countOfThreads <= 0)
         {
             throw new ArgumentOutOfRangeException("Count must be positive");
         }
-        cancellationToken = new CancellationTokenSource();  
-        actions = new ConcurrentQueue<Action>(); 
+        cancellationToken = new CancellationTokenSource();
+        actions = new BlockingCollection<Action>();
         threads = new Thread[countOfThreads];
         for (int i = 0; i < threads.Length; i++)
         {
@@ -25,15 +25,14 @@ public class MyThreadPool
         }
     }
 
-    public IMyTask<TResult> Submit<TResult> (Func<TResult> function)
+    public IMyTask<TResult> Submit<TResult>(Func<TResult> function)
     {
         if (!cancellationToken.IsCancellationRequested)
         {
-            lock(lockObject)
-            {   
+            lock (lockObject)
+            {
                 var task = new MyTask<TResult>(function, this);
-                actions.Enqueue(task.Run);
-                autoReset.Set();
+                actions.Add(task.Run);
                 return task;
             }
         }
@@ -45,29 +44,38 @@ public class MyThreadPool
 
     public void ShutDown()
     {
-
+        lock (lockObject)
+        {
+            cancellationToken.Cancel();
+            actions.CompleteAdding();
+        }
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
     }
 
     private Thread CreateThread()
     {
         var thread = new Thread(() =>
-        {   
-            autoReset.WaitOne();
-            if (actions.TryDequeue(out var action))
+        {
+            foreach (var action in actions.GetConsumingEnumerable())
             {
                 action();
             }
-        }
-        );
+        })
+        {
+            IsBackground = true
+        };
         return thread;
     }
 
     private class MyTask<TResult> : IMyTask<TResult>
-    {   
+    {
         private Exception caughtException;
         private Func<TResult> function;
         private MyThreadPool threadPool;
-        private object lockObj = new object();   
+        private object lockObj = new object();
         private TResult result;
         private ManualResetEvent isCalculated = new ManualResetEvent(false);
         private ConcurrentQueue<Action> continuationOfTask = new ConcurrentQueue<Action>();
@@ -82,15 +90,15 @@ public class MyThreadPool
 
         public TResult Result
         {
-            get 
-            {   
+            get
+            {
                 if (!IsCompleted)
                 {
                     isCalculated.WaitOne();
                 }
                 if (caughtException != null)
                 {
-                    throw new AggregateException(caughtException);  
+                    throw new AggregateException(caughtException);
                 }
                 return result;
             }
@@ -98,19 +106,20 @@ public class MyThreadPool
 
         public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> continuationFunction)
         {
-            if(threadPool.cancellationToken.IsCancellationRequested)
+            if (threadPool.cancellationToken.IsCancellationRequested)
             {
                 throw new InvalidOperationException();
             }
-            lock(lockObj)
+            lock (lockObj)
             {
                 if (IsCompleted)
                 {
-                    threadPool.Submit(() => continuationFunction(result));
+                    return threadPool.Submit(() => continuationFunction(result));
                 }
                 var newContinuationOfTask = new MyTask<TNewResult>(() => continuationFunction(result), threadPool);
-                continuationOfTask.Enqueue(newContinuationOfTask.Run);
-                return newContinuationOfTask;   
+                var action = newContinuationOfTask.Run;
+                continuationOfTask.Enqueue(action);
+                return newContinuationOfTask;
             }
         }
 
@@ -127,23 +136,16 @@ public class MyThreadPool
                     {
                         if (continuationOfTask.TryDequeue(out var taskRun))
                         {
-                            threadPool.actions.Enqueue(taskRun); 
+                            threadPool.actions.Add(taskRun);
                         }
                     }
                 }
                 catch (Exception ex)
                 {
-                    caughtException = ex;   
+                    caughtException = ex;
                 }
 
             }
         }
-
-
-
-        
-        
     }
-
-    
 }
