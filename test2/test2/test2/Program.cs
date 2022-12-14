052609f59efa40a176681cac13ad5579a5268bb9
rongirl <gro.pretty.arabella@mail.ru>
2022-12-14 12:53:23 +0300

    inital commit

---
+++ test2/test2/test2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Chat;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            int port = int.Parse(args[0]);
+            var server = new Server(port);
+            server.Start();
+        }
+        else if (args.Length == 2)
+        {
+            string ip = args[0];    
+            int port = int.Parse(args[1]);
+            var client = new Client(port,  ip);
+            client.Start();
+        }
+    }
+
+}
